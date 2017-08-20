0c9425dd5b745081af3fc6630fb5bbadb225fd7d
Nafies Luthfi <nafiesl@gmail.com>
2017-08-20 21:42:26 +0800

    Test the command creates correct model class content

--- tests/CrudMakeCommandTest.php
+++ tests/CrudMakeCommandTest.php
@@ -27,7 +27,33 @@ class CrudMakeCommandTest extends TestCase
 
         exec('rm '.app_path('Item.php'));
         exec('rm -r '.app_path('Http'));
-        exec('rm '.$migrationFilePath);
+        exec('rm '.database_path('migrations/*'));
+        exec('rm -r '.resource_path('views/items'));
+        exec('rm -r '.base_path('tests/Feature'));
+        exec('rm -r '.base_path('tests/Unit'));
+    }
+
+    /** @test */
+    public function it_creates_correct_model_class_content()
+    {
+        $this->artisan('make:crud', ['name' => 'Item', '--no-interaction' => true]);
+
+        $this->assertFileExists(app_path('Item.php'));
+        $modelClassContent = "<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    //
+}
+";
+        $this->assertEquals($modelClassContent, file_get_contents(app_path('Item.php')));
+        exec('rm '.app_path('Item.php'));
+        exec('rm -r '.app_path('Http'));
+        exec('rm '.database_path('migrations/*'));
         exec('rm -r '.resource_path('views/items'));
         exec('rm -r '.base_path('tests/Feature'));
         exec('rm -r '.base_path('tests/Unit'));
