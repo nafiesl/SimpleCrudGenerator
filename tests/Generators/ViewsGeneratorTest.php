4d367dba78ecb65ae84849798428306cb04af190
Nafies Luthfi <nafiesl@gmail.com>
2018-08-07 13:19:40 +0800

    Update index stubs view and lang stub
    Add search placeholder on master index page
    Add search_text key on master lang file

--- tests/Generators/ViewsGeneratorTest.php
+++ tests/Generators/ViewsGeneratorTest.php
@@ -33,7 +33,7 @@ class ViewsGeneratorTest extends TestCase
         <div class=\"panel panel-default table-responsive\">
             <div class=\"panel-heading\">
                 {{ Form::open(['method' => 'get','class' => 'form-inline']) }}
-                {!! FormField::text('q', ['value' => request('q'), 'label' => __('{$this->lang_name}.search'), 'class' => 'input-sm']) !!}
+                {!! FormField::text('q', ['label' => __('{$this->lang_name}.search'), 'placeholder' => __('{$this->lang_name}.search_text'), 'class' => 'input-sm']) !!}
                 {{ Form::submit(__('{$this->lang_name}.search'), ['class' => 'btn btn-sm']) }}
                 {{ link_to_route('{$this->table_name}.index', __('app.reset')) }}
                 {{ Form::close() }}
